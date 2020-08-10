d13986110675ae3e4a0c234814be64f9bdf1443a
Øyvind Rønningstad <oyvind.ronningstad@nordicsemi.no>
2020-08-10 14:07:06 +0200

    Revert "platform: nordic_nrf: Add plat_test, including nrf_board.h files per board."
    This reverts commit a1aa4c2a21624f3fe9dc5bce58e7f6461bd5ab8c.

--- platform/ext/nrf9160dk_nrf9160.cmake
+++ platform/ext/nrf9160dk_nrf9160.cmake
@@ -6,6 +6,4 @@
 #-------------------------------------------------------------------------------
 
 set(PLATFORM_DIR ${CMAKE_CURRENT_LIST_DIR})
-
-embedded_include_directories(PATH "${PLATFORM_DIR}/target/nordic_nrf/boards/${TARGET_PLATFORM}")
 include("${PLATFORM_DIR}/target/nordic_nrf/nrf9160/nrf9160.cmake")
