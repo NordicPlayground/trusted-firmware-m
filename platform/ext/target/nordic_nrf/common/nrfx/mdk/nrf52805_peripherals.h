b39ab2d84717ffc56feaf2ec6ad24ca0c0d36d85
Andrzej Głąbek <andrzej.glabek@nordicsemi.no>
2020-09-14 11:14:46 +0200

    platform: nordic_nrf: Update nrfx to version 2.3.0
    Update nrfx to the recently released version. See
    https://github.com/NordicSemiconductor/nrfx/blob/v2.3.0/CHANGELOG.md
    for a list of changes that this version introduces.
    
    Origin: nrfx
    License: BSD 3-Clause
    URL: https://github.com/NordicSemiconductor/nrfx/tree/v2.3.0
    commit: 6c31cb7d0e4038afa1efc9bfed40206adfd7b83e
    Purpose: Provide peripheral drivers for Nordic SoCs
    Maintained-by: External
    
    Signed-off-by: Andrzej Głąbek <andrzej.glabek@nordicsemi.no>

--- platform/ext/target/nordic_nrf/common/nrfx/mdk/nrf52805_peripherals.h
+++ platform/ext/target/nordic_nrf/common/nrfx/mdk/nrf52805_peripherals.h
@@ -30,8 +30,8 @@ POSSIBILITY OF SUCH DAMAGE.
 
 */
 
-#ifndef _NRF5340_NETWORK_PERIPHERALS_H
-#define _NRF5340_NETWORK_PERIPHERALS_H
+#ifndef _NRF52805_PERIPHERALS_H
+#define _NRF52805_PERIPHERALS_H
 
 
 /* Clock Peripheral */
@@ -42,58 +42,42 @@ POSSIBILITY OF SUCH DAMAGE.
 #define POWER_PRESENT
 #define POWER_COUNT 1
 
+#define POWER_FEATURE_RAM_REGISTERS_PRESENT
+#define POWER_FEATURE_RAM_REGISTERS_COUNT       3
+
 /* Non-Volatile Memory Controller */
 #define NVMC_PRESENT
 #define NVMC_COUNT 1
-
-#define NVMC_FEATURE_CACHE_PRESENT
-
-/* Voltage request peripheral */
-#define VREQCTRL_PRESENT
-#define VREQCTRL_COUNT 1
-
-/* Volatile Memory Controller Peripheral */
-#define VMC_PRESENT
-#define VMC_COUNT 1
-
-#define VMC_FEATURE_RAM_REGISTERS_PRESENT
-#define VMC_FEATURE_RAM_REGISTERS_COUNT 4
 
 /* Systick timer */
 #define SYSTICK_PRESENT
 #define SYSTICK_COUNT 1
 
-/* Inter-Processor Communication */
-#define IPC_PRESENT
-#define IPC_COUNT 1
-
-#define IPC_CH_NUM 16
-#define IPC_CONF_NUM 16
-#define IPC_GPMEM_NUM 2
+/* Software Interrupts */
+#define SWI_PRESENT
+#define SWI_COUNT 6
 
 /* GPIO */
 #define GPIO_PRESENT
-#define GPIO_COUNT 2
+#define GPIO_COUNT 1
 
 #define P0_PIN_NUM 32
-#define P1_PIN_NUM 16
 
 #define P0_FEATURE_PINS_PRESENT 0xFFFFFFFFUL
-#define P1_FEATURE_PINS_PRESENT 0x0000FFFFUL
 
+/* MPU and BPROT */
+#define BPROT_PRESENT
 
-/* ACL */
-#define ACL_PRESENT
-#define ACL_REGIONS_COUNT 8
+#define BPROT_REGIONS_SIZE 4096
+#define BPROT_REGIONS_NUM 48
 
 /* Radio */
 #define RADIO_PRESENT
 #define RADIO_COUNT 1
 
-#define RADIO_EASYDMA_MAXCNT_SIZE 9
-#define RADIO_FEATURE_IEEE_802_15_4_PRESENT
+#define RADIO_EASYDMA_MAXCNT_SIZE 8
 
-#define RADIO_TXPOWER_TXPOWER_Max RADIO_TXPOWER_TXPOWER_0dBm
+#define RADIO_TXPOWER_TXPOWER_Max RADIO_TXPOWER_TXPOWER_Pos4dBm
 
 /* Accelerated Address Resolver */
 #define AAR_PRESENT
@@ -109,18 +93,21 @@ POSSIBILITY OF SUCH DAMAGE.
 #define CCM_PRESENT
 #define CCM_COUNT 1
 
-/* Distributed Peripheral to Peripheral Interconnect */
-#define DPPI_PRESENT
-#define DPPI_COUNT 1
+/* Peripheral to Peripheral Interconnect */
+#define PPI_PRESENT
+#define PPI_COUNT 1
 
-#define DPPI_CH_NUM 16
-#define DPPI_GROUP_NUM 6
+#define PPI_CH_NUM 10
+#define PPI_FIXED_CH_NUM 12
+#define PPI_GROUP_NUM 6
+#define PPI_FEATURE_FORKS_PRESENT
 
 /* Event Generator Unit */
 #define EGU_PRESENT
-#define EGU_COUNT 1
+#define EGU_COUNT 2
 
 #define EGU0_CH_NUM 16
+#define EGU1_CH_NUM 16
 
 /* Timer/Counter */
 #define TIMER_PRESENT
@@ -130,15 +117,15 @@ POSSIBILITY OF SUCH DAMAGE.
 #define TIMER1_MAX_SIZE 32
 #define TIMER2_MAX_SIZE 32
 
-#define TIMER0_CC_NUM 8
-#define TIMER1_CC_NUM 8
-#define TIMER2_CC_NUM 8
+#define TIMER0_CC_NUM 4
+#define TIMER1_CC_NUM 4
+#define TIMER2_CC_NUM 4
 
 /* Real Time Counter */
 #define RTC_PRESENT
 #define RTC_COUNT 2
 
-#define RTC0_CC_NUM 4
+#define RTC0_CC_NUM 3
 #define RTC1_CC_NUM 4
 
 /* RNG */
@@ -153,43 +140,73 @@ POSSIBILITY OF SUCH DAMAGE.
 #define TEMP_PRESENT
 #define TEMP_COUNT 1
 
-/* Universal Asynchronous Receiver-Transmitter with DMA */
-#define UARTE_PRESENT
-#define UARTE_COUNT 1
-
-#define UARTE0_EASYDMA_MAXCNT_SIZE 16
+/* Serial Peripheral Interface Master */
+#define SPI_PRESENT
+#define SPI_COUNT 1
 
 /* Serial Peripheral Interface Master with DMA */
 #define SPIM_PRESENT
 #define SPIM_COUNT 1
 
 #define SPIM0_MAX_DATARATE  8
+#define SPIM1_MAX_DATARATE  8
 
 #define SPIM0_FEATURE_HARDWARE_CSN_PRESENT  0
+#define SPIM1_FEATURE_HARDWARE_CSN_PRESENT  0
 
 #define SPIM0_FEATURE_DCX_PRESENT  0
+#define SPIM1_FEATURE_DCX_PRESENT  0
 
 #define SPIM0_FEATURE_RXDELAY_PRESENT  0
+#define SPIM1_FEATURE_RXDELAY_PRESENT  0
 
-#define SPIM0_EASYDMA_MAXCNT_SIZE 16
+#define SPIM0_EASYDMA_MAXCNT_SIZE 14
+#define SPIM1_EASYDMA_MAXCNT_SIZE 14
 
 /* Serial Peripheral Interface Slave with DMA*/
 #define SPIS_PRESENT
 #define SPIS_COUNT 1
 
-#define SPIS0_EASYDMA_MAXCNT_SIZE 16
+#define SPIS0_EASYDMA_MAXCNT_SIZE 14
+#define SPIS1_EASYDMA_MAXCNT_SIZE 14
+
+/* Two Wire Interface Master */
+#define TWI_PRESENT
+#define TWI_COUNT 1
 
 /* Two Wire Interface Master with DMA */
 #define TWIM_PRESENT
 #define TWIM_COUNT 1
 
-#define TWIM0_EASYDMA_MAXCNT_SIZE 16
+#define TWIM0_EASYDMA_MAXCNT_SIZE 14
 
 /* Two Wire Interface Slave with DMA */
 #define TWIS_PRESENT
 #define TWIS_COUNT 1
 
-#define TWIS0_EASYDMA_MAXCNT_SIZE 16
+#define TWIS0_EASYDMA_MAXCNT_SIZE 14
+
+/* Universal Asynchronous Receiver-Transmitter */
+#define UART_PRESENT
+#define UART_COUNT 1
+
+/* Universal Asynchronous Receiver-Transmitter with DMA */
+#define UARTE_PRESENT
+#define UARTE_COUNT 1
+
+#define UARTE0_EASYDMA_MAXCNT_SIZE 14
+
+/* Quadrature Decoder */
+#define QDEC_PRESENT
+#define QDEC_COUNT 1
+
+/* Successive Approximation Analog to Digital Converter */
+#define SAADC_PRESENT
+#define SAADC_COUNT 1
+
+#define SAADC_EASYDMA_MAXCNT_SIZE 15
+
+#define SAADC_CH_NUM 8
 
 /* GPIO Tasks and Events */
 #define GPIOTE_PRESENT
@@ -200,12 +217,5 @@ POSSIBILITY OF SUCH DAMAGE.
 #define GPIOTE_FEATURE_SET_PRESENT
 #define GPIOTE_FEATURE_CLR_PRESENT
 
-/* Software Interrupts */
-#define SWI_PRESENT
-#define SWI_COUNT 4
 
-/* Mutex*/
-#define MUTEX_PRESENT
-#define MUTEX_COUNT 1
-
-#endif      // _NRF5340_NETWORK_PERIPHERALS_H
+#endif      // _NRF52805_PERIPHERALS_H
